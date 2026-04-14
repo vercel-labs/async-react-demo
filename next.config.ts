19fa33de41f1e3e8b6797b1cd94b7ed0f40aa6af
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-14 22:08:47 +0200

    Add staletimes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
 };
 
 export default nextConfig;
