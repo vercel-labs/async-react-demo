fb7d28a49c5d50f3c4c95b6fc430bf113c8fd5ae
Aurora Scharff <aurora.sofie@gmail.com>
2026-05-15 22:48:05 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   cacheComponents: true,
   typedRoutes: true,
+  experimental: {
+    useOffline: true,
+    varyParams: true,
+    prefetchInlining: true,
+    optimisticRouting: true,
+    cachedNavigations: true,
+  },
 };
 
 export default nextConfig;
